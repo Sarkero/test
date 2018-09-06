c64972401b012b0b5d3bcf40add2cb6a39d7ed71
Zheng Wang <33861412+Sarkero@users.noreply.github.com>
2018-09-06 17:00:49 +0800

    Add files via upload

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN
+#define MAIN
+
+
+#include <opencv2/opencv.hpp>
+#include <sys/time.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <thread>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <zbar.h>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+
+#include <stddef.h>
+#include <stdlib.h>
+
+
+using namespace cv;
+using namespace std;
+using namespace zbar;
+
+
+#endif // MAIN
